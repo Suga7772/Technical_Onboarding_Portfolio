11c13bedf9b8e49ca270e8c576c8dc27c0e6d618
Mahrukh khan <107424325+Suga7772@users.noreply.github.com>
2023-10-19 12:06:42 +0500

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,9 +20,9 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/woman-with-tablet.jpg";
+import image from "../images/mahrukh-face.jpeg";
 
-const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
+const imageAltText = "Adult female with a smile on her face wearing a dark green button up shirt looking at the camera pointing at her at eye level";
 
 const Home = ({ name, title }) => {
   return (
