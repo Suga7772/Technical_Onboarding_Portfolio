79c1e1b6e538cb1e2a6700bccc1f64d50691d0c5
Mahrukh khan <107424325+Suga7772@users.noreply.github.com>
2023-10-19 12:13:36 +0500

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,26 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/ducks-flying.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "purple hue ducks flying in the sky in a v formation";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Computer Science student studying at National University of Modern Languages, Pakistan. I am an active open source contributor and passionate about neural networks and Generative AI";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
+   "Machine Learning",
+  "Neural Networks",
+  "Project Management",
   "Mobile user interfaces",
-  "Graphic design",
+  "Python",
 ];
 
 /**
@@ -45,7 +44,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in a community enriched environment. By leveraging my community and technical experience I continually look for opportunities to make tech accessible by all.";
 
 const About = () => {
   return (
